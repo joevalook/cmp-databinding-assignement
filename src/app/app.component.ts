4c0c385a47517520da4ce206cb3c0ab81a9534cf
Joe Valookaran <joevalookaran@hotmail.com>
2022-06-20 16:24:17 -0400

    finished up the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+
+  onIntervalFired(event: number) {
+    if (event % 2 === 0) {
+      this.evenNumbers.push(event);
+    } else {
+      this.oddNumbers.push(event);
+    }
+
+
+  }
 }
